148b1862da50404d3e128bb70bab35c9d9f1c348
ronald-colina <ronldco@gmail.com>
2021-12-14 15:23:56 -0300

    animation

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
-})
+});
+
+window.addEventListener ("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; 
+});
